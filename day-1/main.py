594b739a11afeccf00e3d14aa94c8bd431d28003
Joseph Watts <josephcw@live.com>
2020-12-05 13:13:57 -0600

    Complete day 1, puzzle 1

---
+++ day-1/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# find two entries that sum to 2020 and then multiply those numbers together
+def main(input):
+    for i in input:
+        for j in input:
+            if i+j == 2020:
+                print(f'i: {i}, j:{j}, mult: {i*j}')
+                # 'clean' way would be to only iterate half way throught the list forwards
+                # and half way throught the list backwards
+                return
+
+
+if __name__ == '__main__':
+    f = open('input.txt', 'r')
+    input = [line.replace('\n', '') for line in f.readlines()]
+    valid_ints = [int(line) for line in input if line]
+    main(input=valid_ints)
+
